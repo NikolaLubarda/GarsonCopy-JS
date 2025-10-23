05d66fa745e8207b1409996e6c215c50f07f177d
NikolaLubarda <lubarda.nikola9@gmail.com>
2025-10-23 23:56:33 +0200

    Prvi stranica, izbor konobara i local storage vezan za izbor konobara

---
+++ index.js
@@ -0,0 +1,21 @@
+function izborPadajuciMeni() {
+  document.getElementById("PLista").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".PMbtn")) {
+    let dropdowns = document.getElementsByClassName("PL-konobari");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropDown = dropdowns[i];
+      if (openDropDown.classList.contains("show")) {
+        openDropDown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function izabraniKonobar(konobar) {
+  localStorage.setItem("konobar", konobar);
+  console.log(localStorage.getItem("konobar"));
+}
