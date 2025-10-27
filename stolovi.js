0c3587e7656bb92a6ecd4e687c3f74a82de6be98
NikolaLubarda <lubarda.nikola9@gmail.com>
2025-10-27 18:07:23 +0100

    Dadavnje izborne stranice, local storage za izabrani stoo

--- stolovi.js
+++ stolovi.js
@@ -1,3 +1,5 @@
-function dodavanje() {
-  console.log("sto je dodan");
+function dodavanje(sto) {
+  localStorage.setItem("sto", sto);
+  console.log(localStorage.getItem("sto"));
+  window.location.href = "./narudzba.html";
 }
