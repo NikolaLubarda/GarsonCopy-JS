0dcfb6cdcca11ab48f6bebb6c3ded1a34e963c87
NikolaLubarda <lubarda.nikola9@gmail.com>
2025-11-16 22:50:18 +0100

    Poziv za jela API

---
+++ apiPoziv.js
@@ -0,0 +1,24 @@
+const meals = ["Arrabiata", "Beef Lo Mein", "Chicken Handi"];
+
+async function ucitavanjeHrane() {
+  const res = [];
+
+  for (let meal of meals) {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.meals && data.meals.length > 0) {
+        res.push(data.meals[0]);
+      } else {
+        console.warn(`Nema rezultata za: ${meal}`);
+      }
+    } catch (err) {
+      console.error("API greska", err);
+    }
+  }
+  localStorage.setItem("hrana", JSON.stringify(res));
+  console.log("sacuvano:", res);
+}
+
+ucitavanjeHrane();
